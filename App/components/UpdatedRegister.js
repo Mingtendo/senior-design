eff0a8220a23179b93e8330fb3cb5e72cce7256b
Ryan Stachelczyk <ryan.stachelczyk@uconn.edu>
2021-02-19 12:52:26 -0500

    Updated changes

--- App/components/UpdatedRegister.js
+++ App/components/UpdatedRegister.js
@@ -70,18 +70,17 @@ export default function App({ navigation }) {
     return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <View style={styles.container}>
-                <Image style={styles.image} source={require("./assets/Ustudy.png")} />
+                {/* <Image style={styles.image} source={require("./assets/Ustudy.png")} /> */}
 
                 <Text style={styles.joinUs}>Join UStudy</Text>
-
-                <StatusBar style="auto" />
 
                 <View style={styles.inputView}>
                     <TextInput
                         style={styles.TextInput}
                         placeholder="Name:"
                         placeholderTextColor="#003f5c"
-                        onChangeText={(name) => setdisplayName(name)}
+                        value={displayName}
+                        onChangeText={(displayName) => setdisplayName(displayName)}
                     />
                 </View>
 
@@ -89,7 +88,9 @@ export default function App({ navigation }) {
                     <TextInput
                         style={styles.TextInput}
                         placeholder="Email:"
+                        autoCapitalize="none"
                         placeholderTextColor="#003f5c"
+                        value={email}
                         onChangeText={(email) => setEmail(email)}
                     />
                 </View>
@@ -100,6 +101,8 @@ export default function App({ navigation }) {
                         placeholder="Password:"
                         placeholderTextColor="#003f5c"
                         secureTextEntry={true}
+                        autoCapitalize="none"
+                        value={password}
                         onChangeText={(password) => setPassword(password)}
 
                     />
@@ -110,22 +113,22 @@ export default function App({ navigation }) {
                         style={styles.TextInput}
                         placeholder="Confirm Password:"
                         placeholderTextColor="#003f5c"
+                        autoCapitalize="none"
                         secureTextEntry={true}
-                        onChangeText={(password) => setpasswordConfirm(password)}
+                        value={passwordConfirm}
+                        onChangeText={(passwordConfirm) => setpasswordConfirm(passwordConfirm)}
                     />
                 </View>
 
-                <TouchableOpacity style={styles.signUpBtn}>
-                    <Text style={styles.loginText} onPress={()=>register()}>SIGN UP</Text>
+                <TouchableOpacity style={styles.signUpBtn} onPress={() => register()}>
+                    <Text style={styles.loginText} >SIGN UP</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity>
-                    <Text style={styles.login_button} 
-                    onPress={navigation.navigate('Login')}>
+                    <Text style={styles.login_button}
+                        onPress={navigation.navigate('Login')}>
                         Already have an account? Log In</Text>
                 </TouchableOpacity>
-
-
             </View>
         </TouchableWithoutFeedback>
     );
@@ -133,52 +136,52 @@ export default function App({ navigation }) {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-     },
-  
-     image :{
-      width: 50,
-      height: 50,
-      position: 'absolute',
-      top: 40,
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  
+
+    image: {
+        width: 50,
+        height: 50,
+        position: 'absolute',
+        top: 40,
+    },
+
     joinUs: {
-      position: 'absolute',
-      top: 100,
+        position: 'absolute',
+        top: 100,
     },
-  
+
     inputView: {
-      backgroundColor: "#C2E0F9",
-      borderRadius: 30,
-      width: "70%",
-      height: 45,
-      marginBottom: 10,
+        backgroundColor: "#C2E0F9",
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        marginBottom: 10,
     },
-  
+
     TextInput: {
-      height: 50,
-      flex: 1,
-      padding: 10,
-      marginLeft: 20,
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
     },
-  
+
     login_button: {
-      height: 30,
-      marginBottom: 30,
+        height: 30,
+        marginBottom: 30,
     },
-  
+
     signUpBtn: {
-      width: "80%",
-      borderRadius: 25,
-      height: 50,
-      alignItems: "center",
-      justifyContent: "center",
-      marginTop: 20,
-      backgroundColor: "#2174C3",
+        width: "80%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 20,
+        backgroundColor: "#2174C3",
     },
-  
-  });
+
+});
