bbcc1d29407d70e7da0c6ddad23de4fa4d304b2d
Ryan Stachelczyk <ryan.stachelczyk@uconn.edu>
2021-03-17 18:08:50 -0400

    added "Update Courses" button

--- App/components/Courses.js
+++ App/components/Courses.js
@@ -1,59 +1,84 @@
 import React, { Fragment, useState, useContext } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-import Courses from '../courses.json'
-
 import SearchableDropdown from 'react-native-searchable-dropdown';
+import {Auth} from './AuthContext';
+
+import Courses from '../courses.json'
+import firebase from '../firebase';
+
+
 
 export default function App({ navigation }) {
     const [selectedCourses, setselectedCourses] = useState([]);
+
+    const { user } = useContext(Auth);
+    const currentUser = user.toJSON();
+
+    async function updateCourseList(courses){
+        await firebase.firestore()
+        .collection('PROFILES')
+        .doc(currentUser.uid)
+        .set({
+            courseList: {
+                courses
+            }
+        }, { merge: true });
+    }
+
+
+
     return (
         <View>
             <SearchableDropdown
-                    multi={true}
-                    selectedItems={selectedCourses}
-                    onItemSelect={(item) => {
-                        const updatedCourses = selectedCourses;
-                        updatedCourses.push(item);
-                        setselectedCourses(updatedCourses);
-                    }}
-                    items={Courses.courses}
-                    listProps={
-                        {
-                            nestedScrollEnabled: true,
-                        }
+                multi={true}
+                selectedItems={selectedCourses}
+                onItemSelect={(item) => {
+                    const updatedCourses = selectedCourses;
+                    updatedCourses.push(item);
+                    setselectedCourses(updatedCourses);
+                }}
+                items={Courses.courses}
+                listProps={
+                    {
+                        nestedScrollEnabled: true,
                     }
-                    chip={true}
-                    resetValue={false}
-                    onRemoveItem={(item, index) => {
-                        const updatedCourses = selectedCourses.filter((sitem) => sitem.id !== item.id);
-                        setselectedCourses(updatedCourses);
-                    }}
-                    containerStyle={{ padding: 5 }}
-                    itemStyle={{
-                        padding: 10,
-                        marginTop: 2,
-                        backgroundColor: '#ddd',
-                        borderColor: '#bbb',
-                        borderWidth: 1,
-                        borderRadius: 5,
-                    }}
-                    itemTextStyle={{ color: '#222' }}
-                    itemsContainerStyle={{ maxHeight: 140 }}
-                    textInputProps={
-                        {
-                            placeholder: "placeholder",
-                            underlineColorAndroid: "transparent",
-                            style: {
-                                padding: 12,
-                                borderWidth: 1,
-                                borderColor: '#ccc',
-                                borderRadius: 5,
-                            },
-                            onTextChange: text => console.log(text)
-                        }
-                    } 
-                />
+                }
+                chip={true}
+                resetValue={false}
+                onRemoveItem={(item, index) => {
+                    const updatedCourses = selectedCourses.filter((sitem) => sitem.id !== item.id);
+                    setselectedCourses(updatedCourses);
+                }}
+                containerStyle={{ padding: 5 }}
+                itemStyle={{
+                    padding: 10,
+                    marginTop: 2,
+                    backgroundColor: '#ddd',
+                    borderColor: '#bbb',
+                    borderWidth: 1,
+                    borderRadius: 5,
+                }}
+                itemTextStyle={{ color: '#222' }}
+                itemsContainerStyle={{ maxHeight: 140 }}
+                textInputProps={
+                    {
+                        placeholder: "placeholder",
+                        underlineColorAndroid: "transparent",
+                        style: {
+                            padding: 12,
+                            borderWidth: 1,
+                            borderColor: '#ccc',
+                            borderRadius: 5,
+                        },
+                        onTextChange: text => console.log(text)
+                    }
+                } 
+             />
+             <TouchableOpacity style={styles.loginBtn} onPress={() => updateCourseList(selectedCourses)}>
+                <Text style={styles.loginText}>UPDATE COURSES</Text>
+            </TouchableOpacity>
+            
         </View>
     );
 }
